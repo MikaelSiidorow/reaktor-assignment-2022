f057f7223cd01c8e5e05e836536471daf3d551d8
Mikael Siidorow <mikael.siidorow@aalto.fi>
2022-01-23 00:18:38 +0200

    reduce repeat code and improve readability

--- backend/utils/history.js
+++ backend/utils/history.js
@@ -7,6 +7,80 @@ const Cursor = require('../models/cursor')
 
 const baseUrl = 'https://bad-api-assignment.reaktor.com'
 
+const saveNewUser = async (ab, game) => {
+  if (ab === 'a') {
+    // first time seeing this user
+    // create new user with data
+    const newUser = new User({
+      _id: game.playerA.name,
+      totalGames: 1,
+      totalWins: game.winner === game.playerA.name ? 1 : 0,
+      handCounts: {
+        ROCK: game.playerA.played === 'ROCK' ? 1 : 0,
+        PAPER: game.playerA.played === 'PAPER' ? 1 : 0,
+        SCISSORS: game.playerA.played === 'SCISSORS' ? 1 : 0
+      },
+      games: [
+        game._id
+      ]
+    })
+    //save user
+    await newUser.save()
+  }
+  if (ab === 'b') {
+    // first time seeing this user
+    // create new user with data
+    const newUser = new User({
+      _id: game.playerB.name,
+      totalGames: 1,
+      totalWins: game.winner === game.playerB.name ? 1 : 0,
+      handCounts: {
+        ROCK: game.playerB.played === 'ROCK' ? 1 : 0,
+        PAPER: game.playerB.played === 'PAPER' ? 1 : 0,
+        SCISSORS: game.playerB.played === 'SCISSORS' ? 1 : 0
+      },
+      games: [
+        game._id
+      ]
+    })
+    //save user
+    await newUser.save()
+  }
+}
+
+const updateExistingUser = async (ab, user, game) => {
+  if (ab === 'a') {
+    //player found in db
+    user.totalGames = user.totalGames + 1
+    user.totalWins = game.winner === game.playerA.name ? user.totalWins + 1 : user.totalWins
+    user.handCounts = {
+      ROCK: game.playerA.played === 'ROCK' ? user.handCounts.ROCK + 1 : user.handCounts.ROCK,
+      PAPER: game.playerA.played === 'PAPER' ? user.handCounts.PAPER + 1 : user.handCounts.PAPER,
+      SCISSORS: game.playerA.played === 'SCISSORS' ? user.handCounts.SCISSORS + 1 : user.handCounts.SCISSORS
+    }
+    user.games = [
+      game._id, ...user.games
+    ]
+    //update user data
+    await user.save()
+  }
+  if (ab === 'b') {
+    //player found in db
+    user.totalGames = user.totalGames + 1
+    user.totalWins = game.winner === game.playerB.name ? user.totalWins + 1 : user.totalWins
+    user.handCounts = {
+      ROCK: game.playerB.played === 'ROCK' ? user.handCounts.ROCK + 1 : user.handCounts.ROCK,
+      PAPER: game.playerB.played === 'PAPER' ? user.handCounts.PAPER + 1 : user.handCounts.PAPER,
+      SCISSORS: game.playerB.played === 'SCISSORS' ? user.handCounts.SCISSORS + 1 : user.handCounts.SCISSORS
+    }
+    user.games = [
+      game._id, ...user.games
+    ]
+    //update user data
+    await user.save()
+  }
+}
+
 const sortGames = async () => {
   try {
     //sort games collection by time
@@ -42,7 +116,7 @@ const sortGames = async () => {
 }
 
 const saveData = async (data, index, total) => {
-  //all logic breaks if there's an issue in between saving game and users but not like that would ever happen...
+  //all logic breaks (AKA data differs from API) if there's an issue in between saving game and users but not like that would ever happen...
   try {
     //skip games already stored
     if (await Game.findById(data.gameId)) {
@@ -63,106 +137,33 @@ const saveData = async (data, index, total) => {
       const getA = await User.findById(game.playerA.name)
       const getB = await User.findById(game.playerB.name)
 
-      //rare case where player plays against themselves
-      //only update player once
+      //rare case where player plays against themselves -> only update player once
       if (getA && getB && getA._id === getB._id) {
-        //player found in db
-        getA.totalGames = getA.totalGames + 1
-        getA.totalWins = game.winner === game.playerA.name ? getA.totalWins + 1 : getA.totalWins
-        getA.handCounts = {
-          ROCK: game.playerA.played === 'ROCK' ? getA.handCounts.ROCK + 1 : getA.handCounts.ROCK,
-          PAPER: game.playerA.played === 'PAPER' ? getA.handCounts.PAPER + 1 : getA.handCounts.PAPER,
-          SCISSORS: game.playerA.played === 'SCISSORS' ? getA.handCounts.SCISSORS + 1 : getA.handCounts.SCISSORS
-        }
-        getA.games = [
-          game._id, ...getA.games
-        ]
-        //update user data
-        await getA.save()
-      } else if (game.playerA.name === game.playerB.name) {
+        // player found in db
+        await updateExistingUser('a', getA, game)
+      }
+      else if (game.playerA.name === game.playerB.name) {
         // first time seeing this user
-        //create new user with data
-        const a = new User({
-          _id: game.playerA.name,
-          totalGames: 1,
-          totalWins: game.winner === game.playerA.name ? 1 : 0,
-          handCounts: {
-            ROCK: game.playerA.played === 'ROCK' ? 1 : 0,
-            PAPER: game.playerA.played === 'PAPER' ? 1 : 0,
-            SCISSORS: game.playerA.played === 'SCISSORS' ? 1 : 0
-          },
-          games: [
-            game._id
-          ]
-        })
-        //save user
-        await a.save()
-      } else {
+        // create new user with data
+        await saveNewUser('a', game)
+      }
+      // two different players as expected
+      else {
         if (getA) {
-          //user found in db
-          getA.totalGames = getA.totalGames + 1
-          getA.totalWins = game.winner === game.playerA.name ? getA.totalWins + 1 : getA.totalWins
-          getA.handCounts = {
-            ROCK: game.playerA.played === 'ROCK' ? getA.handCounts.ROCK + 1 : getA.handCounts.ROCK,
-            PAPER: game.playerA.played === 'PAPER' ? getA.handCounts.PAPER + 1 : getA.handCounts.PAPER,
-            SCISSORS: game.playerA.played === 'SCISSORS' ? getA.handCounts.SCISSORS + 1 : getA.handCounts.SCISSORS
-          }
-          getA.games = [
-            game._id, ...getA.games
-          ]
-          //update user data
-          await getA.save()
-        } else {
-          // first time seeing this user
-          //create new user with data
-          const a = new User({
-            _id: game.playerA.name,
-            totalGames: 1,
-            totalWins: game.winner === game.playerA.name ? 1 : 0,
-            handCounts: {
-              ROCK: game.playerA.played === 'ROCK' ? 1 : 0,
-              PAPER: game.playerA.played === 'PAPER' ? 1 : 0,
-              SCISSORS: game.playerA.played === 'SCISSORS' ? 1 : 0
-            },
-            games: [
-              game._id
-            ]
-          })
-          //save user
-          await a.save()
-        }
-        if (getB) {
-          //user found in db
-          getB.totalGames = getB.totalGames + 1
-          getB.totalWins = game.winner === game.playerB.name ? getB.totalWins + 1 : getB.totalWins
-          getB.handCounts = {
-            ROCK: game.playerB.played === 'ROCK' ? getB.handCounts.ROCK + 1 : getB.handCounts.ROCK,
-            PAPER: game.playerB.played === 'PAPER' ? getB.handCounts.PAPER + 1 : getB.handCounts.PAPER,
-            SCISSORS: game.playerB.played === 'SCISSORS' ? getB.handCounts.SCISSORS + 1 : getB.handCounts.SCISSORS
-          }
-          getB.games = [
-            game._id, ...getB.games
-          ]
-          //update user data
-          await getB.save()
+          // user found in db
+          await updateExistingUser('a', getA, game)
         } else {
           // first time seeing this user
           // create new user with data
-          const b = new User({
-            _id: game.playerB.name,
-            totalGames: 1,
-            totalWins: game.winner === game.playerB.name ? 1 : 0,
-            handCounts: {
-              ROCK: game.playerB.played === 'ROCK' ? 1 : 0,
-              PAPER: game.playerB.played === 'PAPER' ? 1 : 0,
-              SCISSORS: game.playerB.played === 'SCISSORS' ? 1 : 0
-            },
-            games: [
-              game._id
-            ]
-          })
-          //save user
-          await b.save()
+          await saveNewUser('a', game)
+        }
+        if (getB) {
+          // user found in db
+          await updateExistingUser('b', getB, game)
+        } else {
+          // first time seeing this user
+          // create new user with data
+          await saveNewUser('b', game)
         }
       }
     }
@@ -190,12 +191,12 @@ const getAll = async (cursor = '/rps/history') => {
     }
     // if cursor is found already scanned page is skipped
     else if (cursor !== '/rps/history' && !(await Cursor.findById(cursor))) {
-      const cursorToSave = new Cursor({ _id: cursor })
+      //const cursorToSave = new Cursor({ _id: cursor })
       for (let [index, game] of response.data.data.entries()) {
         await saveData(game, index, total)
       }
       // save cursor to db after fully scanning the page
-      try {await cursorToSave.save()} catch (err) { logger.error(err) }
+      //try {await cursorToSave.save()} catch (err) { logger.error(err) }
     } else {
       console.log('already fetched page data: skipping...')
     }
